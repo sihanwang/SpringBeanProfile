e8a6b63887ab89f6fe785b55e82bbbc5c23ffed2
sihanwang <ocpwj@hotmail.com>
2020-04-16 09:00:38 +0800

    init

---
+++ src/main/java/com/sihan/study/springprofile/SpringBeanProfile/SpringBeanProfileApplication.java
@@ -0,0 +1,13 @@
+package com.sihan.study.springprofile.SpringBeanProfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeanProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeanProfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sihan/study/springprofile/SpringBeanProfile/SpringBeanProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sihan.study.springprofile.SpringBeanProfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeanProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
